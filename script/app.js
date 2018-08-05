cec735df5f31801532441cbad2c67db8fe96cac7
Viktor Grom <viktorgrom84@gmail.com>
2018-08-04 23:37:36 -0700

    initial commit

---
+++ script/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+class FlickrPhoto extends Component {
+    render() {
+        return (
+            <div>
+                <div>Hello World!!!</div>
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(<FlickrPhoto />, document.getElementById('app'));
